139610ff6a2129a3998268bc54575b3f4562c768
Jason Francis <jarfranc@indiana.edu>
2021-02-20 15:53:20 -0500

    Initial commit

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,42 +1,52 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var speed = 0.1
-var Bullet1 = load("res://Bullets/Bullet1.tscn")
-var Bullet2 = load("res://Bullets/Bullet2.tscn")
-onready var Bullets = get_node("/root/Game/Bullets")
+var speed_limit = 10
+var rot = 5
+var speed = 0.2
+var width = 7148
+var height = 2930
+onready var VP = get_viewport_rect().size
 
-
-func _ready():
-	pass
+onready var Enemy = get_node("/root/Game/Enemy")
 
 func _physics_process(_delta):
-	velocity += get_input()*speed
 	position += velocity
-	if position.x >= 1024:
-		velocity = Vector2.ZERO
-		position.x = 1024
-	if position.x <= 0:
-		velocity = Vector2.ZERO
-		position.x = 0
-	if Input.is_action_just_pressed("shoot1"):
-		var bullet1 = Bullet1.instance()
-		bullet1.position = position + Vector2(0, -30)
-		Bullets.add_child(bullet1)
-	if Input.is_action_just_pressed("shoot2"):
-		var bullet2 = Bullet2.instance()
-		bullet2.position = position + Vector2(0, -30)
-		Bullets.add_child(bullet2)
+	velocity += get_input()*speed
+	if velocity.length() > speed_limit:
+		velocity = velocity.normalized() * speed_limit
+	position.x = wrapf(position.x,0,width)
+	position.y = wrapf(position.y,0,height)
+	
+	var distance = position - Enemy.position
+	var ratio_x = abs(distance.x / VP.x)
+	var ratio_y = abs(distance.y / VP.y)
+	var zFactor = ratio_x
+	if ratio_y > ratio_x:
+		zFactor = ratio_y
+	zFactor = clamp(zFactor*2,0.5,3)
+	$Camera2D.zoom = Vector2(zFactor,zFactor)
+			
+		
 	
 
 
 func get_input():
 	var input_vector = Vector2.ZERO
+	$Thrust.hide()
+	if Input.is_action_pressed("up"):
+		$Thrust.show()
+		input_vector.y -= 1
 	if Input.is_action_pressed("left"):
-		input_vector += Vector2(-1,0)
+		rotation_degrees -= rot
 	if Input.is_action_pressed("right"):
-		input_vector += Vector2(1,0)
-	return input_vector
-	
-func die(s):
-	queue_free()
+		rotation_degrees += rot
+	return input_vector.rotated(rotation)
+
+
+func _on_Area2D_body_entered(body):
+	$Area2D/Shield.show()
+
+
+func _on_Area2D_body_exited(body):
+	$Area2D/Shield.hide()
